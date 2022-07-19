9f01b4a6fd5a2899cb53485bbbecf76a9c8c625e
Annette <annettetheodore582@gmail.com>
2022-07-19 15:33:11 +0000

    modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Solution from './components/Solution/Solution';
+import Teams from './components/Teams/Teams';
+import FirstAbout from './components/About/FirstAbout';
+import Footer from './components/Footer/Footer';
+import Copyright from './components/Copyright/Copyright';
+
+
 
 function App() {
   return (
     <div className="App">
     <Header /> 
     <Hero />
+    <FirstAbout />
+    <Solution />
+    <Teams />
+    <Footer />
+    <Copyright />
     </div>
   );
 }
